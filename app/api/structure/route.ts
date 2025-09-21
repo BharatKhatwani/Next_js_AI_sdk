0170c3bc52f3a76b919681c0dbba0c56fdcbaec6
Bharat Khatwani <bharatkhatwani06@gmail.com>
2025-09-21 06:35:56 +0000

    'update the structure backend'

---
+++ app/api/structure/route.ts
@@ -0,0 +1,24 @@
+import {streamObject} from "ai";
+import { google } from "@ai-sdk/google";
+import { NextResponse } from "next/server";
+import {recipeSchema} from "./schema"
+
+export async function Post(req:Request){
+
+    try {
+          const {dish} = await req.json();
+
+  const result =   streamObject({
+         model: google("gemini-2.0-flash"),
+         schema : recipeSchema , 
+      prompt :  `Generate a recipe for ${dish}`,
+    })
+
+    return result.toTextStreamResponse();
+    } 
+  
+    catch (error) {
+        console.error("Error generating recipe", error);
+         return new Response("Failed to generate recipe", {status : 500})
+    }
+}
