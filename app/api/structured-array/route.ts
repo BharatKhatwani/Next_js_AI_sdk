88d06b3d855759b58f9ed7010aa7771ae0eec431
Bharat Khatwani <bharatkhatwani06@gmail.com>
2025-09-21 22:05:44 +0000

    'updated the stuctured-array'

--- app/api/structured-array/route.ts
+++ app/api/structured-array/route.ts
@@ -2,18 +2,20 @@ import { streamObject } from "ai";
 import { google } from "@ai-sdk/google";
 import { pokemonSchema } from "./schema";
 
-export async function POST(req:Request){
-    try {
-        const {type} = await req.json();
-         const result = streamObject({
-        model: google("gemini-2.0-flash"),
+export async function POST(req: Request) {
+  try {
+    const { type } = await req.json();
+
+    const result = streamObject({
+       model: google("gemini-2.0-flash"),
       output: "array",
       schema: pokemonSchema,
       prompt: `Generate a list of 5 ${type} type pokemon`,
     });
 
-    } catch (error) {
-        console.error("Error generating pokemon:", error);
+    return result.toTextStreamResponse();
+  } catch (error) {
+    console.error("Error generating pokemon:", error);
     return new Response("Failed to generate pokemon", { status: 500 });
-    }
+  }
 }
